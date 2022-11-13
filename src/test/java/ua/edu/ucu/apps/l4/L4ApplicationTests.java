bb67178864c2b54f9511f91456545d4491c009e6
Dimitriy Yevchenko <92580268+dimitriyechinho22@users.noreply.github.com>
2022-11-13 23:34:42 +0200

    Update L4ApplicationTests.java

--- src/test/java/ua/edu/ucu/apps/l4/L4ApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/l4/L4ApplicationTests.java
@@ -3,7 +3,7 @@ package ua.edu.ucu.apps.l4;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = L4ApplicationTests.class)
 class L4ApplicationTests {
 
     @Test
